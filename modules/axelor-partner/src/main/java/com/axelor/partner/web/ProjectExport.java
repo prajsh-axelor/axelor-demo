c6a0b936e629eabba7689161b35d4c935ab597a3
prajsh <prajsh.axelor@gmail.com>
2019-07-08 11:28:18 +0530

    data export using csv done

--- modules/axelor-partner/src/main/java/com/axelor/partner/web/ProjectExport.java
+++ modules/axelor-partner/src/main/java/com/axelor/partner/web/ProjectExport.java
@@ -8,13 +8,47 @@ import java.util.List;
 import java.util.Set;
 
 import com.axelor.auth.db.User;
+import com.axelor.inject.Beans;
+import com.axelor.partner.db.Partner;
 import com.axelor.partner.db.Project;
 import com.axelor.partner.db.ProjectTask;
+import com.axelor.partner.db.repo.ProjectRepository;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
+import com.google.inject.Inject;
 import com.opencsv.CSVWriter;
 
 public class ProjectExport {
+
+  @Inject ProjectRepository projectRepository;
+
+  public void printGrid(ActionRequest request, ActionResponse response) {
+	  System.out.println(request.getContext().keySet());
+	  System.out.println(request.getContext().values());
+  }
+
+  public void createCSVGrid(ActionRequest request, ActionResponse response) {
+    String filePath =
+        "/home/axelor/adk-workspace/axelor-demo/modules/axelor-partner/src/main/resources/data-export/project.csv";
+    File file;
+    CSVWriter writer;
+    try {
+      file = new File(filePath);
+      writer = new CSVWriter(new FileWriter(file));
+
+      writer.writeAll(getDataForCSV()); // to
+
+      System.out.println("running export main");
+      // logic to get objects and write into a csv file
+      writer.close();
+    } catch (IOException e) {
+      // TODO: handle exception
+      System.out.println("IOException arised : " + e.getMessage());
+    } finally {
+
+    }
+    System.out.println("success");
+  }
 
   public void print(ActionRequest request, ActionResponse response) {
     System.out.println("Hello from print method ");
@@ -45,14 +79,43 @@ public class ProjectExport {
     System.out.println(taskList);
   }
 
-  public void createCSV(ActionRequest request, ActionResponse response) {
-    String filePath =
-        "/home/axelor/adk-workspace/axelor-demo/modules/axelor-partner/src/main/resources/data-export/project.csv";
-    File file;
-    CSVWriter writer;
+  public String[] getProjectDetails(Project project) {
+    String partnerInterested = "";
+    if (project.getPartnerSet() != null) {
+      Set<Partner> patSet = project.getPartnerSet();
+      if (patSet != null) {
+        for (Partner tempPartner : patSet) {
+          partnerInterested = partnerInterested + "|" + tempPartner.getId().toString();
+        }
+      }
+    }
 
-    Project project = request.getContext().asType(Project.class);
-    String[] headers = new String[] {"id", "name", "priority", "description", "notes", "parent"};
+    String users = "";
+    if (project.getMemberSet() != null) {
+      Set<User> userSet = project.getMemberSet();
+      if (userSet != null) {
+        for (User tempUser : userSet) {
+          users = users + "|" + tempUser.getId().toString();
+        }
+      }
+    }
+
+    String task = "";
+    if (project.getTaskList() != null) {
+      List<ProjectTask> taskList = project.getTaskList();
+      if (taskList != null) {
+        for (ProjectTask tempTask : taskList) {
+          task = task + "|" + tempTask.getId().toString();
+        }
+      }
+    }
+
+    String parent = "";
+    if (project.getParent() != null) {
+      System.out.println(project.getParent().getId());
+      parent += project.getParent().getId().toString();
+    }
+
     String[] projectDetail =
         new String[] {
           project.getId().toString(),
@@ -60,25 +123,79 @@ public class ProjectExport {
           project.getPriority().toString(),
           project.getDescription(),
           project.getNotes(),
-          project.getParent().getId().toString()
+          parent,
+          users.equals("")?"":users.substring(1),
+          task.equals("")?"":task.substring(1),
+          partnerInterested.equals("")?"":partnerInterested.substring(1)
         };
 
-    List<String[]> exportData = new ArrayList<>();
-    exportData.add(headers);
-    exportData.add(projectDetail);
+    return projectDetail;
+  }
 
+  public List<String[]> getDataForCSV() {
+    List<Project> projectList = projectRepository.all().fetch();
+
+    System.out.println("Project list size :: " + projectList.size());
+
+    String[] headers =
+        new String[] {
+          "id",
+          "name",
+          "priority",
+          "description",
+          "notes",
+          "parent",
+          "users",
+          "tasks",
+          "partnerInterested"
+        };
+
+    List<String[]> projectData = new ArrayList<>();
+    projectData.add(headers);
+
+    for (Project project : projectList) {
+      projectData.add(getProjectDetails(project));
+    }
+    return projectData;
+  }
+
+  public void createCSV(ActionRequest request, ActionResponse response) {
+    String filePath =
+        "/home/axelor/adk-workspace/axelor-demo/modules/axelor-partner/src/main/resources/data-export/project-form.csv";
+    File file;
+    CSVWriter writer;
+    Project project = request.getContext().asType(Project.class);
+    String[] headers =
+        new String[] {
+          "id",
+          "name",
+          "priority",
+          "description",
+          "notes",
+          "parent",
+          "users",
+          "tasks",
+          "partnerInterested"
+        };
+
+    List<String[]> projectData = new ArrayList<>();
+    projectData.add(headers);
+    projectData.add(getProjectDetails(project));
     try {
       file = new File(filePath);
       writer = new CSVWriter(new FileWriter(file));
 
-      writer.writeAll(exportData);
+      writer.writeAll(projectData); // to
+
+      System.out.println("running export main");
       // logic to get objects and write into a csv file
       writer.close();
     } catch (IOException e) {
       // TODO: handle exception
-    	e.getMessage();
+      System.out.println("IOException arised : " + e.getMessage());
     } finally {
 
     }
+    System.out.println("success");
   }
 }
