c17cff45ad8daf76cd08cdbe867863cc92cedecc
prajsh <prajsh.axelor@gmail.com>
2019-07-13 18:16:25 +0530

    on 13th july with entities for chart

---
+++ modules/axelor-partner/src/main/java/com/axelor/demoimpl/web/SaleOrderController.java
@@ -0,0 +1,16 @@
+package com.axelor.demoimpl.web;
+
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+
+public class SaleOrderController {
+	
+	public void onConfirm(ActionRequest request, ActionResponse response) {
+		System.out.println("On change in Confirmed method called" ); 
+	}
+	
+	public void calculate(ActionRequest request, ActionResponse response){
+		System.out.println("calculate method called" ); 
+	}
+	
+}
---
+++ modules/axelor-partner/src/main/java/com/axelor/partner/web/ITranslation.java
@@ -0,0 +1,6 @@
+package com.axelor.partner.web;
+
+public class ITranslation {
+	
+	public static final String PROJECT_FULLNAME = /*$$(*/ "Full Name" /*)*/;
+}
